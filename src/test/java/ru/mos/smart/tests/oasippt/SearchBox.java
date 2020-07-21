058ec36810f34b0312f18d2ad064f01155289807
Amidoshka <amidoshka@gmail.com>
2020-07-21 23:01:12 +0300

    reestr

--- src/test/java/ru/mos/smart/tests/oasippt/SearchBox.java
+++ src/test/java/ru/mos/smart/tests/oasippt/SearchBox.java
@@ -4,15 +4,11 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import ru.mos.smart.tests.TestBase;
-
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Selectors.byLinkText;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
@@ -28,21 +24,14 @@ class SearchBox extends TestBase {
     @Test
     @Description("Проверка наличия в витрине поля для поиска")
     void SearchBox() {
-        open("https://smart-predprod.mos.ru/main/#/app/actions");
+        step("Авторизация", () -> {
+            openUrlWithAuthorization("", LOGIN_s_PRAVAMI, PASSWORD_s_PRAVAMI);
+        });
 
-            $(byText("Войти по логину и паролю")).click();
-            $("#username").setValue(LOGIN_s_PRAVAMI);
-            $("#password").setValue(PASSWORD_s_PRAVAMI);
-            $("#kc-login").click();
-
-        step("Открытие в навигаторе Настройки - Конструктор витрин", ()-> {
-                    $(byLinkText("Настройки")).click();
-                    $(byLinkText("Конструктор витрин")).click();
-                });
-
-        step("Открытие в навигаторе Настройки - Конструктор витрин", ()-> {
-            $(By.name("candidateSearchValue")).setValue("Запросы проверки версии Проекта планировки").pressEnter();
-            open("https://smart-predprod.mos.ru/extras/#/app/showcase-builder/edit/bd3098c7-6ae7-414d-92d1-012bda1129b5");
-                });
+        step("Открытие в навигаторе Настройки - Конструктор витрин", () -> {
+            $(byText("Настройки")).click();
+            $(byText("Конструктор витрин")).click();
+        });
+        $(byName("candidateSearchValue")).setValue("Запросы проверки версии Проекта планировки").pressEnter();
     }
 }
