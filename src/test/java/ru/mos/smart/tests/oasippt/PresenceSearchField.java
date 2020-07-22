f11cf13e80b233af4f4de627d8481e5f8f2c7d25
gabdrakhimova_er <gabdrahimovaer@krlb.ru>
2020-07-22 18:43:40 +0500

    Open Register

--- src/test/java/ru/mos/smart/tests/oasippt/PresenceSearchField.java
+++ src/test/java/ru/mos/smart/tests/oasippt/PresenceSearchField.java
@@ -12,8 +12,7 @@ import ru.mos.smart.tests.TestBase;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static ru.mos.smart.helpers.DriverHelper.openUrlWithAuthorization;
 
@@ -23,10 +22,10 @@ import static ru.mos.smart.helpers.DriverHelper.openUrlWithAuthorization;
 @Tag("OASI")
 @Tag("ECP")
 
-class SearchBox extends TestBase {
+class PresenceSearchField extends TestBase {
     @Test
     @Description("Проверка наличия в витрине поля для поиска")
-    void SearchBox() {
+    void PresenceSearchField() {
         step("Авторизация", () -> {
             openUrlWithAuthorization("", LOGIN_s_PRAVAMI, PASSWORD_s_PRAVAMI);
         });
@@ -35,6 +34,7 @@ class SearchBox extends TestBase {
             $(byText("Настройки")).click();
             $(byText("Конструктор витрин")).click();
         });
+
         $(byName("candidateSearchValue")).setValue("Запросы проверки версии Проекта планировки").pressEnter();
         $(byTitle("Редактировать витрину")).click();
         $(".nav-item:nth-child(3) span:nth-child(2)").click();
