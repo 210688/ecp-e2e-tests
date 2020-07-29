1cae48be5d8ddeceba3466b315b3f20e9bf1db04
gabdrakhimova_er <gabdrahimovaer@krlb.ru>
2020-07-29 14:09:13 +0500

    Open The Application Form LRP

---
+++ src/test/java/ru/mos/smart/tests/ugd/lrp/openTheApplicationFormLRP.java
@@ -0,0 +1,57 @@
+package ru.mos.smart.tests.ugd.lrp;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import ru.mos.smart.tests.TestBase;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static ru.mos.smart.helpers.DriverHelper.openUrlWithAuthorization;
+
+@Epic("Платформа ЕЦП")
+@Feature("Модуль \"UGD\"")
+@Story("Проверка открытия формы подачи Подать заявку на участие в конкурсе ЛРП")
+@Tag("UGD")
+@Tag("LRP")
+@Tag("ECP")
+
+class openTheApplicationFormLRP extends TestBase {
+    @Test
+    @Description("Проверка открытия формы подачи Подать заявку на участие в конкурсе ЛРП")
+    void openTheApplicationFormLRP() {
+
+        step("Авторизация", () -> {
+            openUrlWithAuthorization("", LOGIN_UGD, PASSWORD_UGD);
+        });
+
+        step("Открытие в навигаторе Мои возможности", () -> {
+            //В левом боковом меню выбрать «Госуслуги и функции» > «Возможности»
+            $(byLinkText("Госуслуги и функции")).click();
+            $(byLinkText("Возможности")).click();
+        });
+
+        step("Выбрать операцию «Подать заявку на участие в конкурсе ЛРП».", () -> {
+            //в поисковой строке ввести "Подать заявку на участие в конкурсе ЛРП"
+            $(byClassName("form-control")).setValue("Подать заявку на участие в конкурсе ЛРП").pressEnter();
+
+            //выбрать операцию "Подать заявку на участие в конкурсе ЛРП"
+            $(byLinkText("Подать заявку на участие в конкурсе ЛРП")).click();
+        });
+
+        step("Проверка наличия вкладок", () -> {
+            $(byText("Выбор участника конкурса")).shouldBe(visible);
+            $(byText("Сведения об участнике конкурса")).shouldBe(visible);
+            $(byText("Сведения об организации")).shouldBe(visible);
+            $(byText("Критерии номинации")).shouldBe(visible);
+            $(byText("Загрузка файлов")).shouldBe(visible);
+        });
+    }
+}
