f04dac7dd3a7246fc2b1a8362245c331eac24cf3
gabdrakhimova_er <gabdrahimovaer@krlb.ru>
2020-08-03 15:24:43 +0500

    Сatalogs MTSK

---
+++ src/test/java/ru/mos/smart/tests/ugd/mtsk/catalogsMTSK.java
@@ -0,0 +1,48 @@
+package ru.mos.smart.tests.ugd.mtsk;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import ru.mos.smart.tests.TestBase;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static ru.mos.smart.helpers.DriverHelper.openUrlWithAuthorization;
+
+@Epic("Платформа ЕЦП")
+@Feature("Модуль \"UGD\"")
+@Story("Проверка наличия каталогов МТСК")
+@Tag("UGD")
+@Tag("MTSK")
+@Tag("ECP")
+
+class catalogsMTSK extends TestBase {
+    @Test
+    @Description("Проверка наличия каталогов МТСК")
+    void catalogsMTSK() {
+
+        step("Авторизация", () -> {
+            openUrlWithAuthorization("", LOGIN_UGD, PASSWORD_UGD);
+        });
+
+        step("Развернуть в навигаторе блок МТСК", () -> {
+            $(byLinkText("МТСК")).click();
+        });
+
+        step("Проверка наличия каталогов", () -> {
+            $(byText("Инновации")).shouldBe(visible);
+            $(byText("Документы")).shouldBe(visible);
+            $(byText("Услуги")).shouldBe(visible);
+            $(byText("Организации")).shouldBe(visible);
+            $(byText("Строительные ресурсы")).shouldBe(visible);
+            $(byText("Проектные решения")).shouldBe(visible);
+        });
+    }
+}
