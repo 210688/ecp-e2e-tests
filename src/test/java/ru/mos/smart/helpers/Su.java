c4f35e1553b43e652a735bbb6d16bd8867d78121
soldatov_k <soldatovks@krlb.ru>
2020-08-06 15:25:07 +0300

    suite

--- src/test/java/ru/mos/smart/helpers/Su.java
+++ src/test/java/ru/mos/smart/helpers/Su.java
@@ -5,12 +5,13 @@ import io.qameta.allure.LabelAnnotation;
 import java.lang.annotation.*;
 
 import static io.qameta.allure.util.ResultsUtils.STORY_LABEL_NAME;
+import static io.qameta.allure.util.ResultsUtils.SUITE_LABEL_NAME;
 
 @Documented
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
-@LabelAnnotation(name = STORY_LABEL_NAME)
+@LabelAnnotation(name = SUITE_LABEL_NAME)
 public @interface Su {
 
     String value();
