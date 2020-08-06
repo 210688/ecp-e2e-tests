3c32dcb5d376f6336529db64b5c9393bc3424df1
soldatov_k <soldatovks@krlb.ru>
2020-08-06 15:03:56 +0300

    suite

---
+++ src/test/java/ru/mos/smart/helpers/Su.java
@@ -0,0 +1,18 @@
+package ru.mos.smart.helpers;
+
+import io.qameta.allure.LabelAnnotation;
+
+import java.lang.annotation.*;
+
+import static io.qameta.allure.util.ResultsUtils.STORY_LABEL_NAME;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@LabelAnnotation(name = STORY_LABEL_NAME)
+public @interface Su {
+
+    String value();
+
+}
