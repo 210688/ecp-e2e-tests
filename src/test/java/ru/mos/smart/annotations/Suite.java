22dc522450e80098101dc3736dbce9414b0dc2ba
soldatov_k <soldatovks@krlb.ru>
2020-08-13 15:54:35 +0300

    Добавление новых аннотаций

--- src/test/java/ru/mos/smart/annotations/Suite.java
+++ src/test/java/ru/mos/smart/annotations/Suite.java
@@ -7,6 +7,10 @@ import java.lang.annotation.*;
 import static io.qameta.allure.util.ResultsUtils.STORY_LABEL_NAME;
 import static io.qameta.allure.util.ResultsUtils.SUITE_LABEL_NAME;
 
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
 @LabelAnnotation(name = "suite")
 public @interface Suite {
 
