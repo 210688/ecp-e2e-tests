4724a4a0e0769f7c63cb4239ccb31420bed47c04
soldatov_k <soldatovks@krlb.ru>
2020-08-18 11:32:30 +0300

    Обновил код

--- src/test/java/ru/mos/smart/tests/mgz/openProjectsCard.java
+++ src/test/java/ru/mos/smart/tests/mgz/openProjectsCard.java
@@ -1,11 +1,10 @@
 package ru.mos.smart.tests.mgz;
 
 import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import ru.mos.smart.annotations.Layer;
 import ru.mos.smart.tests.TestBase;
 
 import static com.codeborne.selenide.Condition.visible;
@@ -15,6 +14,7 @@ import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 import static ru.mos.smart.pages.LoginPage.openUrlWithAuthorization;
 
+@Layer("web")
 @Epic("MGZ (Мосгорзаказ)")
 @Tag("mgz") @Tag("all_test")
 class openProjectsCard extends TestBase {
