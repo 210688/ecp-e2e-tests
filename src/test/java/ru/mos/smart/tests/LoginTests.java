f58936841dafaf0ff29aead575ebd06bfe04be1f
soldatov_k <soldatovks@krlb.ru>
2020-08-27 08:55:48 +0300

    review code

--- src/test/java/ru/mos/smart/tests/LoginTests.java
+++ src/test/java/ru/mos/smart/tests/LoginTests.java
@@ -1,15 +1,12 @@
 package ru.mos.smart.tests;
 
-import com.codeborne.selenide.Condition;
 import io.qameta.allure.Epic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
@@ -32,8 +29,8 @@ class LoginTests extends TestBase {
         });
 
         step("Проверка успешной авторизации", ()-> {
-            $(byText("test test test"))
-                    .waitUntil(visible, 20000).shouldBe(visible);
+            $(byText("Правительство Москвы")) //test test test
+                    .waitUntil(visible, 10000).shouldBe(visible);
 
         });
     }
