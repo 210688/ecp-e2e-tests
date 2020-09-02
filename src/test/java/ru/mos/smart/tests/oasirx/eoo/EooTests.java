44e9112bcd0127dcc65b07ee8c29b8f491949a83
Amidoshka <amidoshka@gmail.com>
2020-09-02 22:48:17 +0300

    Добавление ЭОО(общественные обсуждения)

---
+++ src/test/java/ru/mos/smart/tests/oasirx/eoo/EooTests.java
@@ -0,0 +1,26 @@
+package ru.mos.smart.tests.oasirx.eoo;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import ru.mos.smart.annotations.Layer;
+import ru.mos.smart.tests.TestBase;
+
+import static ru.mos.smart.pages.LoginPage.openUrlWithAuthorization;
+
+@Layer("web")
+@Epic("OASIRX (ОАСИ Рефактор-Икс)")
+@Feature("EOO (Электронные общественные обсуждения)")
+@Tag("oasirx") @Tag("eoo")
+class EooTests extends TestBase {
+
+    @Test
+    @DisplayName("Проверка открытия формы подачи \"Подать заявку на участие в конкурсе ЛРП\"")
+    void openTheApplicationFormLrp() {
+        openUrlWithAuthorization("", LOGIN_SZZ, PASSWORD_SZZ);
+
+    }
+
+}
