3a78f2ba01e49b936bfdd196352f2860a8efde2a
soldatov_k <soldatovks@krlb.ru>
2020-09-22 15:36:07 +0300

    обновление кода

--- src/test/java/ru/mos/smart/pages/EooPagesButton.java
+++ src/test/java/ru/mos/smart/pages/EooPagesButton.java
@@ -5,12 +5,15 @@ import static com.codeborne.selenide.Selenide.$;
 public class EooPagesButton {
 
 
-    public static void switchTask() {
-        $(".fa-lg").click(); // треугольничек нажатие перейти к задачи
+    public static void switchTask(int Index) {
+
+        $(".fa-lg", Index).click(); // треугольничек нажатие перейти к задачи
     }
+
     public static void takeTask(){ // Взять в работу назначенным исполнителем
         $(".btn-sm:nth-child(2)").click(); // взять в работу кнопка
     }
+
     public static void  saveButtonNextTask(){  //Сохранить и передать на подготовку
         $("#next").click();
     }
