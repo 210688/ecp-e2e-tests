645175b3cde0aa435f81f681c2315a30dc9c68a2
Amidoshka <amidoshka@gmail.com>
2020-10-11 23:53:12 +0300

    PlatformTests

--- src/test/java/ru/mos/smart/tests/cdp/PlatformTests.java
+++ src/test/java/ru/mos/smart/tests/cdp/PlatformTests.java
@@ -6,28 +6,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import ru.mos.smart.annotations.Layer;
+import ru.mos.smart.pages.LoginPage;
 import ru.mos.smart.tests.TestBase;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
-import static ru.mos.smart.pages.LoginPage.openUrlWithAuthorization;
+
 
 public class PlatformTests {
 
-}
     @Layer("web")
     @Epic("CDP (ЕЦП_платформа)")
     @Tag("cdp")
     class SupportRequest extends TestBase {
 
-    @Test
-    @Description("Используется тестовый пользователь 89 c нужными правами")
-    @DisplayName("Проверка отправки уведомления в тех подержку")
-    void SupportRequestForward() {
-        openUrlWithAuthorization("", LOGIN_FEEDBACK, PASSWORD_FEEDBACK); //авторизация в системе
-        step("Открытие ссылки Инициировать обращение в техническую поддержку", () -> {
+        @Test
+        @Description("Используется тестовый пользователь 89 c нужными правами")
+        @DisplayName("Проверка отправки уведомления в тех подержку")
+        void SupportRequestForward() {
+            LoginPage.openUrlWithAuthorization("", login_ecp, password_ecp); //авторизация в системе
+            step("Открытие ссылки Инициировать обращение в техническую поддержку", () -> {
 
-});
+            });
+        }
     }
 }
