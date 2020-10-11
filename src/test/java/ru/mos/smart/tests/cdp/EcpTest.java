3be7ba2be15b611427e45ce1f2ea68efcf368b69
Amidoshka <amidoshka@gmail.com>
2020-10-12 00:00:58 +0300

    PlatformTests

---
+++ src/test/java/ru/mos/smart/tests/cdp/EcpTest.java
@@ -0,0 +1,20 @@
+package ru.mos.smart.tests.cdp;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import ru.mos.smart.annotations.Layer;
+import ru.mos.smart.pages.LoginPage;
+import ru.mos.smart.tests.TestBase;
+
+import static io.qameta.allure.Allure.step;
+
+@Layer("web")
+@Epic("CDP (ЕЦП_платформа)")
+@Tag("cdp")
+public class EcpTest extends TestBase {
+    @Test
+
+}
