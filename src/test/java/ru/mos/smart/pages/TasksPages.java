686690ba88c172332ee1ff54db607308842fb428
Васенков Станислав Олегович <svasenkov@krlb.ru>
2020-10-18 00:17:51 +0300

    Update TasksPages.java

--- src/test/java/ru/mos/smart/pages/TasksPages.java
+++ src/test/java/ru/mos/smart/pages/TasksPages.java
@@ -31,16 +31,16 @@ public class TasksPages {
 
 	@Step("Выбор задачи")
 	public static void searchTask(String tasksName) {
-		$(By.xpath("//a[@href='/main/#/app/tasks']")) //Задачи
+		$x("//a[@href='/main/#/app/tasks']") // Задачи
 				.waitUntil(visible, 5000).click();
 
 		step("Ввод наименования задачи в поиске", () -> {
-			$(By.xpath("//input[@placeholder='Найти задачу']"))
-					.waitUntil(visible, 5000).shouldBe(visible).setValue(tasksName).pressEnter();
+			$x("//input[@placeholder='Найти задачу']")
+					.waitUntil(visible, 10000).setValue(tasksName).pressEnter();
 		});
 
 		step("Выбор задачи в списке {taskName}", ()-> {
-			$(By.xpath("//span[contains(@class,'task-title') and contains(.,'" + tasksName + "')]")).click();
+			$x("//span[contains(@class,'task-title') and contains(.,'" + tasksName + "')]").click();
 		});
 	}
 }
