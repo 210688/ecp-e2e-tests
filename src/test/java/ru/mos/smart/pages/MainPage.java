b6516065977202b2ccc8eb95d47e0d2152d8fdf0
Васенков Станислав Олегович <svasenkov@krlb.ru>
2020-10-18 00:16:03 +0300

    Update MainPage.java

--- src/test/java/ru/mos/smart/pages/MainPage.java
+++ src/test/java/ru/mos/smart/pages/MainPage.java
@@ -4,7 +4,7 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 import static io.qameta.allure.Allure.step;
 
 /**
@@ -16,13 +16,13 @@ public class MainPage {
     @Step("Возможности")
     public static void actions() {
         step("Возможности", ()-> {
-            $(By.xpath("//nav//a[@href='/main/#/app/actions']"))
+            $x("//nav//a[@href='/main/#/app/actions']")
                     .shouldBe(visible).click();
         });
     }
 
     @Step("ЭОО")
     public static void dobavitEoo() {
-        $(By.xpath("//nav//a//span[text()='ЭОО']")).click();
+        $x("//nav//a//span[text()='ЭОО']").click();
     }
 }
