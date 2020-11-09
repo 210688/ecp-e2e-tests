283ee41a796e947d666a334ed0e67949787461aa
gabdrakhimova_er <gabdrahimovaer@krlb.ru>
2020-11-09 14:57:33 +0500

    fr

--- src/test/java/ru/mos/smart/tests/fr/LaunchProjectTests.java
+++ src/test/java/ru/mos/smart/tests/fr/LaunchProjectTests.java
@@ -25,22 +25,18 @@ public class LaunchProjectTests extends TestBase {
         openUrlWithAuthorization("", LOGIN_FR, PASSWORD_FR);
 
         step("В панели \"Навигатор\" нажать кнопку \"Объекты АИП\"", () -> {
-            sleep(10000);
             open("/fr/#/app/aip/list");
         });
 
         step("В рабочей области витрины нажать на кнопку \"Создать Объект АИП\"", () -> {
-            sleep(10000);
             $(byLinkText("Создать объект АИП")).click();
         });
 
         step("Заполнить поля", () -> {
-            sleep(10000);
-            $(".form-control").setValue("Тест 03.11");
+            $(".form-control").setValue("Автотест");
         });
 
         step("Нажать на кнопку \"Сохранить\"", () -> {
-            sleep(5000);
             $("#submit").click();
         });
     }
