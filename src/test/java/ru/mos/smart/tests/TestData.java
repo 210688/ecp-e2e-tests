ba7e0bf1b2c48c3f26c7f5ea1457e30a0b9d5bd5
soldatovks <soldatovks@krlb.ru>
2020-12-16 22:22:20 +0300

    reformat code

--- src/test/java/ru/mos/smart/tests/TestData.java
+++ src/test/java/ru/mos/smart/tests/TestData.java
@@ -4,92 +4,10 @@ package ru.mos.smart.tests;
 public class TestData {
     public static String
 
-            login_test = "user_tests",
-            password_test = "8QKo5bi",
 
-    login_drone_test = "login_drone_test",
-            password_drone_test = "l0RipC7",
+            //todo prod Login and password
 
-    LOGIN_GIS_TEST = "gis_test",
-            PASSWORD_GIS_TEST = "P@ssw0rd",
-
-    LOGIN_MGZ = "ivanov_mg",
-            PASSWORD_MGZ = "Passsword1",
-
-    LOGIN_SZZ = "ivanov_sz",
-            PASSWORD_SZZ = "Passsword1",
-
-    loginEoo = "elektronnye_oo",
-            passwordEoo = "9zWjDx8",
-
-    LOGIN_AGR_PKR = "ivanov_ap",
-            PASSWORD_AGR_PKR = "Passsword1",
-
-    LOGIN_BLAGO = "ivanov_b",
-            PASSWORD_BLAGO = "Passsword1",
-
-    LOGIN_DDX = "ivanov_dd",
-            PASSWORD_DDX = "Passsword1",
-
-    LOGIN_FASAD = "ivanov_f",
-            PASSWORD_FASAD = "Passsword1",
-
-    LOGIN_OO = "ivanov_po",
-            PASSWORD_OO = "Passsword1",
-
-    LOGIN_MRGP = "mrgp_i",
-            PASSWORD_MRGP = "Passsword1",
-
-    LOGIN_ELMA = "elma_i",
-            PASSWORD_ELMA = "Passsword1",
-
-    LOGIN_HEARINGS = "ivanov_khe",
-            PASSWORD_HEARINGS = "Passsword1",
-
-    LOGIN_ITMKA = "itmka_i",
-            PASSWORD_ITMKA = "Passsword1",
-
-    login_pmt = "pmt_i",
-            pasword_pmt = "Passsword1",
-
-    LOGIN_DPIOS = "dpioos_i",
-            PASSWORD_DPIOS = "Passsword1",
-
-    LOGIN_SHERBINKA = "shcherbinka_i",
-            PASSWORD_SHERBINKA = "Passsword1",
-
-    LOGIN_TROITCK = "troitck_i",
-            PASSWORD_TROITCK = "Passsword1",
-
-    LOGIN_OSSIG = "ossig_i",
-            PASSWORD_OSSIG = "Passsword1",
-
-    LOGIN_VMMDO = "vmmdo_i",
-            PASSWORD_VMMDO = "Passsword1",
-
-    LOGIN_UGD = "ugd_i",
-            PASSWORD_UGD = "Passsword1",
-
-    LOGIN_APGR = "apgr_i",
-            PASSWORD_APGR = "Passsword1",
-
-    LOGIN_FEEDBACK = "89",
-            PASSWORD_FEEDBACK = "8t7YHdf",
-
-    LOGIN_FR = "ivanov_fr",
-            PASSWORD_FR = "P@ssw0rd",
-
-    DEFAULT_EMAIL = "ak@krlb.ru",
-            DEFAULT_PHONE = "",
-            DEFAULT_SNILS = "",
-            DEFAULT_PASSWORD = "1qazxsw2",
-
-    LOGIN_MR_TEST = "mr_test",
-            PASSWORD_MR_TEST = "P@ssw0rd",
-
-    //todo prod Login and password
-
-    loginAll = "Users_all_tests",
+            loginAll = "Users_all_tests",
             passwordAll = "ddR53Ku",
 
     loginOsi = "Users_osi_tests",
