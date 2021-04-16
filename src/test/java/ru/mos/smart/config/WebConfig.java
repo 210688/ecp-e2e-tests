56a137f9ccb582e7ec4613d136e5e1753c6c423a
soldatovks <soldatovks@krlb.ru>
2021-04-16 13:03:44 +0300

    add

--- src/test/java/ru/mos/smart/config/WebConfig.java
+++ src/test/java/ru/mos/smart/config/WebConfig.java
@@ -2,7 +2,7 @@ package ru.mos.smart.config;
 
 import org.aeonbits.owner.Config;
 
-//@Config.LoadPolicy(Config.LoadType.MERGE)
+//@Config.LoadPolicy(Config.LoadType.MERGE)/
 @Config.Sources({
         //"system.properties",
         "classpath:${environment}.properties",
