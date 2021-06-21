77e891300fd220cfc39a8c7088ea5a383e7d4f81
John Smoker <testgits@yandex.ru>
2021-06-21 20:20:57 +0500

    test #3085, 3086, 4128

--- src/test/java/ru/mos/smart/tests/ugd/ssr/UgdSsrTests.java
+++ src/test/java/ru/mos/smart/tests/ugd/ssr/UgdSsrTests.java
@@ -116,4 +116,92 @@ public class UgdSsrTests extends TestBase {
             });
         });
     }
+
+    @Test
+    @AllureId("3085")
+    @DisplayName("Проверка доступности реестра Администрирование рабочих дней помощи в переезде")
+    @Tags({@Tag("predprod"), @Tag("prod"), @Tag("allModules"), @Tag("regress")})
+    @Epic("UGD (УГД)")
+    @Feature("SSR (Суперсервис реновации ССР)")
+    void administrationWorkingDaysTest() {
+        AuthorizationPage.openUrlWithAuthorization("", webConfig().loginUgd(), webConfig().passwordUgd());
+        NavigatorPage.reestrPage();
+
+        step("Найти и открыть реестр Администрирование рабочих дней помощи в переезде", () -> {
+            $(byName("candidateSearchValue")).setValue("Администрирование рабочих дней помощи в переезде").pressEnter();
+            $(byText("Администрирование рабочих дней помощи в переезде")).click();
+        });
+        step("Отображается реестр Администрирование рабочих дней помощи в переезде", () -> {
+            $("h2[_ngcontent-c4]").shouldHave(text("Администрирование рабочих дней помощи в переезде"));
+            $(".catalog-showcase-wrapper").$$("tr[_ngcontent-c19]").shouldHave(sizeGreaterThan(0));
+        });
+    }
+
+    @Test
+    @AllureId("3086")
+    @DisplayName("Проверка доступности реестра Реестр заявлений на помощь в переезде")
+    @Tags({@Tag("predprod"), @Tag("prod"), @Tag("allModules"), @Tag("regress")})
+    @Epic("UGD (УГД)")
+    @Feature("SSR (Суперсервис реновации ССР)")
+    void shippingApplicationTest() {
+        AuthorizationPage.openUrlWithAuthorization("", webConfig().loginUgd(), webConfig().passwordUgd());
+        NavigatorPage.reestrPage();
+
+        step("Найти и открыть реестр Реестр заявлений на помощь в переезде", () -> {
+            $(byName("candidateSearchValue")).setValue("Реестр заявлений на помощь в переезде").pressEnter();
+            $(byText("Реестр заявлений на помощь в переезде")).click();
+        });
+
+        step("Отображается реестр Реестр заявлений на помощь в переезде", () -> {
+            $("h2[_ngcontent-c4]").shouldHave(text("Реестр заявлений на помощь в переезде"));
+            $(".catalog-showcase-wrapper").$$("tr[_ngcontent-c19]").shouldHave(sizeGreaterThan(0));
+        });
+    }
+
+    @Test
+    @AllureId("4128")
+    @DisplayName("Проверка возможности подписания договора и акта приема-передачи")
+    @Tags({@Tag("predprod"), @Tag("prod"), @Tag("allModules"), @Tag("regress")})
+    @Epic("UGD (УГД)")
+    @Feature("SSR (Суперсервис реновации ССР)")
+    void reestrSigningTest() {
+        AuthorizationPage.openUrlWithAuthorization("", webConfig().loginUgd(), webConfig().passwordUgd());
+        NavigatorPage.reestrPage();
+
+        step("Открыть реестр ССР. Реестр жителей", () -> {
+            $(byName("candidateSearchValue")).setValue("ССР. Реестр жителей").pressEnter();
+            $(byText("ССР. Реестр жителей")).click();
+        });
+        step("Открыть карточку жителя, нажав на поле с ФИО жителя", () -> {
+            $("showcase-builder-runtime a").click();
+            switchTo().window(1);
+        });
+        step("Перейти на вкладку Возможности", () -> {
+            $("app-standard-header").shouldBe(visible);
+            $$(".nav-link").findBy(text("Возможности")).click();
+        });
+        step("Открыть возможность Внести сведения о подписании договора", () -> {
+            $$("td[_ngcontent-c6]").findBy(text("Внести сведения о подписании договора")).click();
+        });
+        step("В модальном окне с предупреждением нажать на кнопку Ок", () -> {
+            $(".modal-content button").click();
+        });
+        step("Проверка  перехода в возможность 'Внести сведения о подписании договора'", () ->
+                $$(".wrapper-content h3[_ngcontent-c13]").shouldHave(textsInAnyOrder(
+                        "Общие сведения",
+                        "Сведения о принятых заявлениях на согласие/отказ",
+                        "Шаг 1. Распечатайте и подпишите договор",
+                        "Шаг 2. Распечатайте и подпишите акт приема-передачи жилого помещения",
+                        "Шаг 3. Загрузите подписанные документы - договор и акт приема-передачи жилого помещения",
+                        "Сведения о собственниках / нанимателях",
+                        "Принять решение")));
+        step("Активировать чекбокс Акт будет подписан позже", () -> {
+            $(byName("actWillBeSignedLater")).$("div.green").click();
+            $(byText("Подписанный акт")).$(".our-mandatory").shouldNotBe(visible);
+        });
+        step("Нажать на кнопку Отмена", () -> {
+            $$("button[_ngcontent-c13]").findBy(text("Отмена")).click();
+            $("app-standard-header").shouldBe(visible);
+        });
+    }
 }
