b95b3985f2376d9551eadd6c3e58530924a99a18
John Smoker <testgits@yandex.ru>
2021-06-28 22:20:16 +0500

    test #4807

--- src/test/java/ru/mos/smart/tests/mkasdprv/MkasdprvApplicantDetailsCheck.java
+++ src/test/java/ru/mos/smart/tests/mkasdprv/MkasdprvApplicantDetailsCheck.java
@@ -1,6 +1,7 @@
 package ru.mos.smart.tests.mkasdprv;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.AllureId;
 import io.qameta.allure.Epic;
@@ -21,7 +22,8 @@ import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 import static ru.mos.smart.config.ConfigHelper.webConfig;
 
@@ -126,6 +128,86 @@ public class MkasdprvApplicantDetailsCheck extends TestBase {
         step("В шапке задачи нажать на номер заявления", () -> {
             $(byName("docNumber")).click();
             $("h1").shouldHave(text("Карточка заявления"));
+        });
+    }
+
+    @Test
+    @AllureId("4807")
+    @DisplayName("03. Принять решение по заявлению - отказ. Причины отказа")
+    @Tags({@Tag("predprod"), @Tag("mkasdprv")})
+    @Epic("MKASDPRV (МКА Вывески)")
+    @Feature("Автотесты")
+    void negativeDecisionTest() {
+        AuthorizationPage.openUrlWithAuthorization("", webConfig().loginMka(), webConfig().passwordMka());
+        //TasksPage.takeUnusedTask("Проверить данные заявления");
+        TasksPage.openTaskByDocumentName("КВ-2021-2375");
+        step("В поле «Принять решение по заявлению» выбрать радиобаттон «Отказать в приеме документов»", () -> {
+            $("[for=Refuse]").parent().$("input").click();
+            $("[placeholder=Причина]").click();
+        });
+        step("В поле «Причина отказа» в выпадающем списке проверить наличие 3 причин отказа:", () -> {
+            $("ng-dropdown-panel").$(byText(
+                    "Представление заявителем запроса и документов," +
+                            " подписанных с использованием электронной подписи," +
+                            " не принадлежащей лицу, подавшему запрос"))
+                    .shouldBe(visible);
+            $("ng-dropdown-panel").$(byText(
+                    "Представление заявителем неполного комплекта документов," +
+                            " предусмотренных пунктом 2.5.1.1 Регламента в качестве документов," +
+                            " подлежащих обязательному представлению заявителем"))
+                    .shouldBe(visible);
+            $("ng-dropdown-panel").$(byText(
+                    "Электронные копии (электронные образы) документов," +
+                            " необходимых для предоставления государственной услуги," +
+                            " не поддаются прочтению и (или) не соответствуют требованиям к форматам их предоставления"))
+                    .shouldBe(visible);
+        });
+        step("Проверить добавление нового блока причин отказа, с помощью кнопки «+»", () -> {
+            $$(byText("Причина отказа")).shouldHave(size(1));
+            $(".fas.fa-plus").click();
+            $$(byText("Причина отказа")).shouldHave(size(2));
+            $(".fas.fa-minus").click();
+            $$(byText("Причина отказа")).shouldHave(size(1));
+        });
+        step("В поле «Причина отказа» выбрать причину из выпадающего списка", () -> {
+            $("[placeholder=Причина]").click();
+            $("ng-dropdown-panel").$(".ng-option").click();
+        });
+        step("В поле «Комментарий» ввести произвольное текстовое значение", () -> {
+            $("textarea.form-control").setValue("ТЕСТ");
+        });
+        step("Нажать на кнопку «Сформировать файл решения»", () ->
+                $("button.for-reason-btn").click());
+        step("Проверить, что сформированный файл можно скачать, подписать, удалить", () -> {
+            $$(".ex-small-file-box").last().shouldHave(text("Отказ в приеме документов.docx"));
+            $$(".ex-small-file-box").last().$(byText("Подписать")).shouldBe(visible);
+            $$(".ex-small-file-box").last().$(byText("Скачать"))
+                    .shouldHave(attributeMatching("href", "(.*)/filestore/v1/files/(.*)?systemCode=mkasdprv"));
+            $$(".ex-small-file-box").last().$(".fa.fa-trash-o").shouldBe(visible);
+        });
+        step("После удаления файла проверить, что можно загрузить файл решения в расширении pdf, doc(x)", () -> {
+            $$(".ex-small-file-box").last().$(".fa.fa-trash-o").click();
+            $(".modal-content").$(".btn-primary").click();
+
+            $("cdp-ex-file-manager.to-check-File").shouldHave(text("Загрузить файл"));
+            $$("input[type='file']").last().uploadFromClasspath("files_for_tests/doc.docx");
+            $("[title='doc.docx']").shouldBe(visible);
+            $$(".ex-small-file-box").last().$(".fa.fa-trash-o").click();
+            $(".modal-content").$(".btn-primary").click();
+            $("[title='doc.docx']").shouldNotBe(visible);
+
+            $("cdp-ex-file-manager.to-check-File").shouldHave(text("Загрузить файл"));
+            Selenide.executeJavaScript("$(\"input[type='file']\").last().val('');");
+            $$("input[type='file']").last().uploadFromClasspath("files_for_tests/pdf.pdf");
+            $("[title='pdf.pdf']").shouldBe(visible);
+            $$(".ex-small-file-box").last().$(".fa.fa-trash-o").click();
+            $(".modal-content").$(".btn-primary").click();
+            $("[title='pdf.pdf']").shouldNotBe(visible);
+
+            $("cdp-ex-file-manager.to-check-File").shouldHave(text("Загрузить файл"));
+            Selenide.executeJavaScript("$(\"input[type='file']\").last().val('');");
+            $$("input[type='file']").last().uploadFromClasspath("files_for_tests/doc.doc");
+            $("[title='doc.doc']").shouldBe(visible);
         });
     }
 }
