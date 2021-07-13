b54a55cf05afa62e8c0969ec0c5168f0ed807dad
svasenkov <aanher@gmail.com>
2021-07-13 13:15:58 +0300

    updated code

--- src/test/java/ru/mos/smart/tests/vgupt/test.java
+++ src/test/java/ru/mos/smart/tests/vgupt/test.java
@@ -1,4 +1,5 @@
 package ru.mos.smart.tests.vgupt;
 
+// Тестов нет, обновлений не выводим, "мертвая система"
 public class test {
 }
--- src/test/java/ru/mos/smart/tests/vmmdo/test.java
+++ src/test/java/ru/mos/smart/tests/vmmdo/test.java
@@ -1,4 +1,5 @@
 package ru.mos.smart.tests.vmmdo;
 
+// Тестов нет, обновлений не выводим, "мертвая система"
 public class test {
 }
