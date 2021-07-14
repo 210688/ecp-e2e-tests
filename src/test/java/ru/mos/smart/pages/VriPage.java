5eace2e2b1b3586f62398ecb42df349e0bce30f4
John Smoker <testgits@yandex.ru>
2021-07-14 20:36:15 +0500

    test #3312

--- src/test/java/ru/mos/smart/pages/VriPage.java
+++ src/test/java/ru/mos/smart/pages/VriPage.java
@@ -1,8 +1,12 @@
 package ru.mos.smart.pages;
 
+import com.codeborne.selenide.CollectionCondition;
+import io.qameta.allure.Step;
+
 import java.util.List;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
@@ -16,10 +20,44 @@ public class VriPage {
         return this;
     }
 
+    @Step("Проверить, что есть вкладки [{tabsList}]")
     public VriPage checkHasTabs(List<String> tabsList) {
         for (String tab : tabsList) {
-            $(".tab-container").$(byText(tab)).shouldBe(visible);
+            step("Видна вкладка " + tab, () ->
+                    $(".tab-container").$(byText(tab)).shouldBe(visible));
         }
+
+        return this;
+    }
+
+    @Step("Нажать на шестеренку")
+    public VriPage cogWheelChoose(String option) {
+        $(".fa.fa-cog").click();
+        $(".additional-menu.dropdown").shouldHave(cssClass("show"));
+        step("Выбрать " + option, () ->
+                $(".additional-menu.dropdown").$(byText(option)).click());
+
+        return this;
+    }
+
+    @Step("Открывается страница с историей изменений в заявке")
+    public VriPage checkChangeLogShown() {
+        $("cdp-document-log-by-date").shouldBe(visible);
+
+        step("Отображается информация о смене статуса заявки, авторе и времени", () -> {
+            $("cdp-document-log-status").shouldBe(visible);
+            $(".table thead").shouldHave(text("Операция"));
+            $(".table thead").shouldHave(text("Дата"));
+            $(".table thead").shouldHave(text("Пользователь"));
+            $(".table tbody").$$("tr").shouldHave(sizeGreaterThan(0));
+        });
+
+        return this;
+    }
+
+    @Step("Открыть первую карточку")
+    public VriPage openFirstCard() {
+        $(".table tbody a").click();
 
         return this;
     }
---
+++ src/test/java/ru/mos/smart/tests/oasirx/vri/VriCardFormTests.java
@@ -0,0 +1,37 @@
+package ru.mos.smart.tests.oasirx.vri;
+
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import ru.mos.smart.pages.AuthorizationPage;
+import ru.mos.smart.pages.NavigatorPage;
+import ru.mos.smart.pages.VriPage;
+import ru.mos.smart.tests.TestBase;
+
+import static ru.mos.smart.config.ConfigHelper.webConfig;
+
+public class VriCardFormTests extends TestBase {
+    @Test
+    @AllureId("3312")
+    @DisplayName("Форма карточки: История изменений")
+    @Story("Работа с реестром и карточкой ВРИ")
+    @Feature("VRI (ВРИ)")
+    @Epic("OASIRX (ОАСИ Рефактор-Икс)")
+    @Tags({@Tag("predprod"), @Tag("prod"), @Tag("regres"), @Tag("oasirx"), @Tag("vri")})
+    void changeHistoryTest() {
+        VriPage vriPage = new VriPage();
+
+        AuthorizationPage.openUrlWithAuthorizationAPI("", webConfig().loginOasirx(), webConfig().passwordOasirx());
+        NavigatorPage.goToVri();
+
+        vriPage
+                .openFirstCard()
+                .cogWheelChoose("История изменений")
+                .checkChangeLogShown();
+    }
+}
