89662c38a0e0d9cf4489ced0d56a99ed93f36b54
soldatovks <soldatovks@krlb.ru>
2021-11-17 23:58:12 +0300

    Добавления API тестов

--- src/test/java/ru/mos/smart/config/ConfigHelper.java
+++ src/test/java/ru/mos/smart/config/ConfigHelper.java
@@ -4,6 +4,30 @@ import org.aeonbits.owner.ConfigFactory;
 
 public class ConfigHelper {
 
+    public static String getAuthorizationURL() {
+        return getConfig().authorizationUrl();
+    }
+
+    public static String getApplicationUrl() {
+        return getConfig().applicationUrl();
+    }
+
+    public static String getUsername() {
+        return getConfig().username();
+    }
+
+    public static String getPassword() {
+        return getConfig().password();
+    }
+
+    public static String getClientSecret() {
+        return getConfig().clientSecret();
+    }
+
+    private static ApiConfig getConfig() {
+        return ConfigFactory.newInstance().create(ApiConfig.class, System.getProperties());
+    }
+
     public static WebConfig webConfig() {
         return ConfigFactory.newInstance().create(WebConfig.class, System.getProperties());
     }
